f7c0c8698230386ec7ddfc3a1fd28120aa132745
Iris Martinez <iris.annelise.martinez@gmail.com>
2025-08-28 21:33:09 -0400

    Start longest increasing subsequence alg

---
+++ chapter2/longincsub.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+// given an input array of integers a, return a sequence of indices that corresponds to the longest increasing subsequence of the array
+// backtracking practice
+typedef struct{
+    int length;
+    int* arr;
+} sequence;
+
+int main() {
+    sequence s1 = {
+        length = 23
+        [3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3,8,4,6,2,6]
+    } 
+    return 0;
+}
+
+int** longestIncreasingSubsequence(sequence s) {
+    if s.length = 0 {
+        return []; 
+    } 
+}
